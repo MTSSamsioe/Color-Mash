1eef66c8939d0df6ff317509099bb4b894aaabf1
M. Samsioe <michael.samsioe@gmail.com>
2022-06-19 14:14:13 +0000

    Add function start()

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+
+
+
+// Code for count down clock and game start button
+
+let clock = 20;
+
+function start() {
+
+    // Sets score to 0
+    document.getElementById("score").innerHTML = 0
+    // hides starta button while function is running
+    document.getElementById('startBtn').style.visibility = "hidden";
+    // show answer buttons
+    document.getElementById("yes").style.visibility = "visible";
+    document.getElementById("no").style.visibility = "visible";
+   
+  
+  // code from line 17 to 26 and line 35 to 38  is heavily influenced from https://www.codegrepper.com/code-examples/javascript/add+countdown+time+to+game+javascript
+  
+  let interval = setInterval(function(){
+      
+  
+  document.getElementById('timeLeft').innerHTML=clock;
+  --clock;
+    if (clock === -2){
+      clearInterval(interval);
+      clock = 20;
+      // Changes time to game over
+      document.getElementById('timeLeft').innerHTML='Game over';
+      
+    
+      // show answer buttons
+      document.getElementById("yes").style.visibility = "hidden";
+      document.getElementById("no").style.visibility = "hidden";
+      // Show start button and hide answerbuttons
+      document.getElementById('startBtn').style.visibility = "visible";
+      //document.getElementsByClassName("btn").style.visibility = "hidden"
+      alert("You're out of time!");
+     
+    }
+  }, 1000);
+}
